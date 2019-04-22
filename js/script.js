ffe9f8e60c4f411595c35e062d98496d215d403c
bolortuya <btumurbaatar@hawk.iit.edu>
2019-04-21 20:37:28 -0500

    finish map js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 
 function initMap() {
 
-  var umbria = {lat: 41.8887936, lng: -87.6313221};
+  var align = {lat:41.894574,lng: -87.607220};
 
-  var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: umbria});
+  var map = new google.maps.Map(document.getElementById('map'),
+   {
+     zoom: 8,
+    center: align,
+    mapTypeId: google.maps.MapTypeId.Map,
+    mapTypeControlOptions: {
+   position: google.maps.ControlPosition.BOTTOM_CENTER
+ }
+   });
 
-  var marker = new google.maps.Marker({position: umbria, map: map});
+  var marker = new google.maps.Marker({position: align, map: map});
 }
 
 window.addEventListener('load', initMap);
