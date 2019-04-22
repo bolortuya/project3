1e08e3d9c228e40ecf198ea7e5a76b42d64fa3e0
bolortuya <btumurbaatar@hawk.iit.edu>
2019-04-21 20:47:55 -0500

    edit js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 
 function initMap() {
-
   var align = {lat:41.894574,lng: -87.607220};
-
   var map = new google.maps.Map(document.getElementById('map'),
    {
      zoom: 8,
-    center: align,
-    mapTypeId: google.maps.MapTypeId.Map,
-    mapTypeControlOptions: {
-   position: google.maps.ControlPosition.BOTTOM_CENTER
- }
+     center: align,
+     mapTypeId: google.maps.MapTypeId.Map,
+     mapTypeControlOptions: {
+       position: google.maps.ControlPosition.BOTTOM_CENTER }
    });
-
   var marker = new google.maps.Marker({position: align, map: map});
 }
-
 window.addEventListener('load', initMap);
